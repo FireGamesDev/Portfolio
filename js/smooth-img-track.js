12ddb4a616935314e01efebefa6f99e42e086e69
FireGamesDev <86660115+FireGamesDev@users.noreply.github.com>
2024-11-19 22:04:50 +0100

    tweaks size

--- js/smooth-img-track.js
+++ js/smooth-img-track.js
@@ -35,7 +35,7 @@ track.onmousemove = e => {
     let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
     // Smooth clamping to prevent abrupt stopping
-    nextPercentage = Math.max(Math.min(nextPercentage, 0), -276.5);
+    nextPercentage = Math.max(Math.min(nextPercentage, 0), -325);
 
     track.dataset.percentage = nextPercentage;
 
